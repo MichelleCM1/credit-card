96f54951a2a36ca83ea83ea750aa058e6b6b1bc5
MICHELLE CAROLINE MARTINS DOS SANTOS <michelle.cmsantos1@senacsp.edu.br>
2024-09-16 20:00:36 -0300

    update

--- src/componets/credit/index.tsx
+++ src/componets/credit/index.tsx
@@ -59,7 +59,7 @@ export function Credit({ cardSide, data }: CreditProps) {
         </View>
 
         <View style={styles.footer}>
-          <Text style={styles.name}> Michelle Martins{data.name}</Text>
+          <Text style={styles.name}>{data.name}</Text>
 
           <View style={styles.flag}>
             <View style={[styles.circle, styles.red]} />
@@ -71,18 +71,18 @@ export function Credit({ cardSide, data }: CreditProps) {
       <Animated.View style={[styles.card, styles.back, backAnimatedStyles]}>
         <View>
           <Text style={styles.label}>Número do cartão</Text>
-          <Text style={styles.value}>{data.number} 9876 5432 1098 7654</Text>
+          <Text style={styles.value}>{data.number}</Text>
         </View>
 
         <View style={styles.footer}>
           <View>
             <Text style={styles.label}>Validade</Text>
-            <Text style={styles.value}>{data.date}12/30</Text>
+            <Text style={styles.value}>{data.date}</Text>
           </View>
 
           <View>
             <Text style={styles.label}>CVV</Text>
-            <Text style={styles.value}>{data.code}123</Text>
+            <Text style={styles.value}>{data.code}</Text>
           </View>
         </View>
       </Animated.View>
